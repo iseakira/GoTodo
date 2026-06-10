7874e658fc9f33762fa039dadf9896e4a7a0d90c
IseAkira <akira.ise.m@gmail.com>
2026-06-10 18:43:05 +0900

    再配置

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,58 +6,42 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/extra/bundebug"
 	_ "github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+
+	"gotodo/internal/model"
 )
-
-
-type Todo struct {
-	bun.BaseModel `bun:"table:todos,alias:t"`
-	ID int64 `bun:"id,pk,autoincrement"`
-	Content string `bun:"content,notnull"`
-	Done bool `bun:"done"`
-	Until time.Time `bun:"until,nullzero"`
-	CreatedAt time.Time
-	UpdateAt time.Time `bun:",nullzero"`
-	DeletedAt time.Time `bun:",soft_delete,nullzero"`
-}
 
 func main() {
 	godotenv.Load()
 
-	sqldb,err := sql.Open("pg",os.Getenv("DATABASE_URL"))
-
+	sqldb, err := sql.Open("pg", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sqldb.Close()
 
-	db := bun.NewDB(sqldb,pgdialect.New())
+	db := bun.NewDB(sqldb, pgdialect.New())
 	db.AddQueryHook(bundebug.NewQueryHook(
 		bundebug.FromEnv("BUNDEBUG"),
 	))
 
-	//contextはキャンセルやタイムアウトなどの処理を行える
-
 	ctx := context.Background()
 
-	_,err = db.NewCreateTable().Model((*Todo)(nil)).IfNotExists().Exec(ctx)
-
+	_, err = db.NewCreateTable().Model((*model.Todo)(nil)).IfNotExists().Exec(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	e := echo.New()
 
-	e.GET("/",func(c echo.Context) error {
-		return c.String(http.StatusOK,"")
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "")
 	})
 	e.Logger.Fatal(e.Start(":8989"))
-
 }
