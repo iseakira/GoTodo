df6e3c5fa35cda8e301fb24fbabe6a9271780bfd
IseAkira <akira.ise.m@gmail.com>
2026-06-10 18:49:33 +0900

    config.goへ集約

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,38 +2,24 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	_ "github.com/uptrace/bun/driver/pgdriver"
-	"github.com/uptrace/bun/extra/bundebug"
 
+	"gotodo/internal/config"
 	"gotodo/internal/model"
 )
 
 func main() {
-	godotenv.Load()
+	config.LoadEnv()
 
-	sqldb, err := sql.Open("pg", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sqldb.Close()
-
-	db := bun.NewDB(sqldb, pgdialect.New())
-	db.AddQueryHook(bundebug.NewQueryHook(
-		bundebug.FromEnv("BUNDEBUG"),
-	))
+	db := config.NewDB()
+	defer db.Close()
 
 	ctx := context.Background()
 
-	_, err = db.NewCreateTable().Model((*model.Todo)(nil)).IfNotExists().Exec(ctx)
+	_, err := db.NewCreateTable().Model((*model.Todo)(nil)).IfNotExists().Exec(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	_ "github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+)
+
+func LoadEnv() {
+	godotenv.Load()
+}
+
+func NewDB() *bun.DB {
+	sqldb, err := sql.Open("pg", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := bun.NewDB(sqldb, pgdialect.New())
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.FromEnv("BUNDEBUG"),
+	))
+
+	return db
+}
